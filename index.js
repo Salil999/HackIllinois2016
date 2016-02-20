6a614cf2e02aecea573e0d6780591c737c64dd61
Salil999 <ssaxen4@illinois.edu>
2016-02-20 17:20:18 -0600

    updated

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ var path = require('path');
 var Firebase = require('firebase');
 var Client = require('node-rest-client').Client;
 
+// Shashank's _id: 56c8ea7f061b2d440baf43dd
+
 function getInfo(url) {
     var client = new Client();
     client.get(url, function(data, res) {
@@ -14,23 +16,18 @@ function getInfo(url) {
 
 var app = express();
 
-//app.use(express.static(path.join(__dirname, 'public')));
-
 app.get('/', function(req, res) {
-    var x = getInfo('http://api.reimaginebanking.com/accounts?type=Checking&key=65be1ee16b2fbd6faade515551491370');
-    res.send(x);
-    getInfo('http://www.mocky.io/v2/56c8da23110000173cf88290');
-    //res.sendFile(path.join(__dirname, 'public/index.html'));
+	console.log(getInfo('http://api.reimaginebanking.com/customers/56c8ea7f061b2d440baf43dd?key=65be1ee16b2fbd6faade515551491370'));
+	res.redirect('/home');
 });
 
-app.post('/login', function(req, res) {
+app.get('/home', function(req, res) {
+	app.use(express.static(path.join(__dirname, 'public')));
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+app.get('/login', function(req, res) {
     res.send('hello world');
-    /*
-	res.send('Hello');
-    console.log(getInfo('http://www.mocky.io/v2/56c8da23110000173cf88290'));
-    var x = getInfo('http://www.mocky.io/v2/56c8da23110000173cf88290');
-    res.send(x);
-    */
 });
 
 app.listen(5000);
