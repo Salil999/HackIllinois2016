0991b90423717f1aff0705a566531920ce187dbc
Salil999 <ssaxen4@illinois.edu>
2016-02-21 00:46:20 -0600

    updated serv files

--- index.js
+++ index.js
@@ -7,28 +7,79 @@ const ref = new Firebase('https://splitsave.firebaseio.com');
 
 const API_KEY = '65be1ee16b2fbd6faade515551491370';
 const baseURL = 'http://api.reimaginebanking.com/';
-const ID = '56c8ea7f061b2d440baf43dd';
+const cust_ID = '56c8ea7f061b2d440baf43dd';
+const acc_ID = '56c8f105061b2d440baf43ed';
 // Shashank's _id: 56c8ea7f061b2d440baf43dd
 var endpoint;
 
 var app = express();
 
 app.get('/', function(req, res) {
-	var client = new Client();
-	var uri = baseURL + 'customers/' + ID + '?key=' + API_KEY;
-	client.get(uri, function(data, res) {
-		console.log(data);
-	});
-	var url = baseURL + 'customers/' + ID + '/accounts' + '?key=' + API_KEY
-	client.get(url, function(data, res) {
-		//console.log(url);
-		console.log(data);
-	});
-	res.redirect('/home');
+    var client = new Client();
+
+    var user = "test@test.com";
+    var pass = "test";
+
+    var uri = baseURL + 'customers/' + cust_ID + '?key=' + API_KEY;
+    console.log(uri);
+    client.get(uri, function(data, res) {
+        //console.log(data);
+        ref.authWithPassword({
+            email: user,
+            password: pass
+        }, function(err, account) {
+            if (err) {
+                console.log(err);
+                return;
+            } else {
+                var usersRef = ref.child('users').child(account.uid).child('account');
+                usersRef.update({
+                    id: data._id,
+                    first_name: data.first_name,
+                    last_name: data.last_name,
+                    personalData: {
+                        city: data.address.city,
+                        state: data.address.state,
+                        street_name: data.address.street_name,
+                        street_number: data.address.street_number,
+                        zip: data.address.zip
+                    }
+                });
+            }
+        });
+    });
+
+    uri = baseURL + 'customers/' + cust_ID + '/accounts' + '?key=' + API_KEY;
+    client.get(uri, function(data, res) {
+        //console.log(data);
+        ref.authWithPassword({
+            email: user,
+            password: pass
+        }, function(err, account) {
+            if (err) {
+                console.log(err);
+                return;
+            } else {
+                var usersRef = ref.child('users').child(account.uid).child('account');
+                usersRef.update({
+                    money: {
+                        /*balance: data[0].balance,*/
+                        balance: 10000,
+                        accNo: data[0].account_number,
+                        type: data[0].type
+                    }
+                });
+            }
+        });
+    });
+
+
+
+    res.redirect('/home');
 });
 
 app.get('/home', function(req, res) {
-	app.use(express.static(path.join(__dirname, 'public')));
+    app.use(express.static(path.join(__dirname, 'public')));
     res.sendFile(__dirname + '/public/index.html');
 });
 
